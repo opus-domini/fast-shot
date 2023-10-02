9f1eb5e826408bfbf2ca5142dc53a1a8df433dd9
Hugo Andrade <contato@hugoandrade.dev>
2023-10-02 14:06:53 -0300

    Add tests

--- request_test.go
+++ request_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -81,6 +82,30 @@ func TestRequest_SetQueryParams(t *testing.T) {
 	}
 }
 
+func TestRequest_SetQueryString(t *testing.T) {
+	// Arrange
+	client := DefaultClient("https://example.com")
+	// Act
+	req := client.GET("/test").
+		SetQueryString("key1=value1&key2=value2")
+	// Assert
+	if req.queryParams.Get("key1") != "value1" || req.queryParams.Get("key2") != "value2" {
+		t.Errorf("SetQueryString failed to set query parameters correctly")
+	}
+}
+
+func TestRequest_SetQueryString_InvalidQuery(t *testing.T) {
+	// Arrange
+	client := DefaultClient("https://example.com")
+	// Act
+	req := client.GET("/test").
+		SetQueryString("%")
+	// Assert
+	if len(req.validations) == 0 {
+		t.Errorf("SetQueryString should append error for invalid query string")
+	}
+}
+
 func TestRequest_Body(t *testing.T) {
 	// Arrange
 	client := DefaultClient("https://example.com")
@@ -107,6 +132,53 @@ func TestRequest_BodyJSON(t *testing.T) {
 	}
 }
 
+func TestRequest_BodyJSON_Error(t *testing.T) {
+	// Arrange
+	client := DefaultClient("https://example.com")
+	body := func() {}
+	// Act
+	r := client.POST("/path").
+		BodyJSON(body)
+	// Assert
+	if r.validate() == nil || !strings.Contains(r.validate().Error(), "failed to marshal JSON") {
+		t.Errorf("BodyJSON didn't capture the marshaling error")
+	}
+}
+
+func TestRequest_createFullURL_Error(t *testing.T) {
+	// Arrange
+	client := DefaultClient(":%^:")
+	r := client.GET("/path")
+	// Act
+	_, err := r.createFullURL()
+	// Assert
+	if err == nil {
+		t.Errorf("createFullURL did not return an error for invalid baseURL")
+	}
+}
+
+func TestRequest_createFullURL_WithQueryParams(t *testing.T) {
+	// Arrange
+	client := DefaultClient("https://example.com")
+	r := client.GET("/path").
+		AddQueryParam("key1", "value1").
+		AddQueryParam("key2", "value2")
+
+	// Act
+	fullURL, err := r.createFullURL()
+
+	// Assert
+	if err != nil {
+		t.Errorf("createFullURL returned an error: %v", err)
+		return
+	}
+
+	expectedURL := "https://example.com/path?key1=value1&key2=value2"
+	if fullURL.String() != expectedURL {
+		t.Errorf("createFullURL returned wrong URL, got: %s, want: %s", fullURL, expectedURL)
+	}
+}
+
 func TestRequest_Send(t *testing.T) {
 	// Arrange
 	server := httptest.NewServer(
@@ -114,7 +186,7 @@ func TestRequest_Send(t *testing.T) {
 			w.WriteHeader(http.StatusOK)
 			_ = json.NewEncoder(w).
 				Encode(map[string]string{
-					"message": "success",
+					"message": "Success!",
 				})
 		}))
 	defer server.Close()
@@ -138,10 +210,21 @@ func TestRequest_Send(t *testing.T) {
 	_ = json.Unmarshal(body, &result)
 
 	// Assert
-	if result["message"] != "success" {
+	if result["message"] != "Success!" {
 		t.Errorf(
 			"Unexpected response: got %v, want 'success'",
 			result["message"],
 		)
+	}
+}
+
+func TestRequest_Send_Error(t *testing.T) {
+	// Arrange
+	client := DefaultClient("https://api.example.com")
+	// Act
+	_, err := client.GET("invalid path").Send()
+	// Assert
+	if err == nil {
+		t.Errorf("Send should return error for invalid URL")
 	}
 }
